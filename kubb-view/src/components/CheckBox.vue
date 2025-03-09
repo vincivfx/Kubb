389f53ca5f87a97ab46e7801ebe458118e9b44ec
Vincenzo Gallina <30446234+vincivfx@users.noreply.github.com>
2025-03-09 23:03:48 +0100

    Added checkbox and (partially working) datetime

---
+++ kubb-view/src/components/CheckBox.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  name: "CheckBox",
+  data: () => ({
+    currentValue: false
+  }),
+  methods: {
+    switchValue() {
+      this.currentValue = !this.currentValue;
+    }
+  },
+  mounted() {
+  }
+}
+</script>
+
+<template>
+  <div class="mv-2">
+    <input style="display: none" v-model="currentValue" type="checkbox" />
+
+    <button :class="['btn checkbox', {'checkbox-active': currentValue}]" class="btn-checkbox" @click="switchValue">
+      <svg v-if="currentValue" viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg">
+        <line x1="5" y1="5" x2="25" y2="25" stroke="black" stroke-width="4"/>
+        <line x1="25" y1="5" x2="5" y2="25" stroke="black" stroke-width="4"/>
+      </svg>
+      &nbsp;
+    </button>
+
+    <label @click="switchValue">
+      <slot/>
+    </label>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- kubb-view/src/components/DateTimeInput.vue
+++ kubb-view/src/components/DateTimeInput.vue
@@ -4,11 +4,62 @@ import InputBlock from "@/components/InputBlock.vue";
 export default {
   name: "DateTimeInput",
   components: {InputBlock},
+  data: () => ({
+    showPicker: false,
+    days: [],
+    
+  }),
+  methods: {
+    /**
+     * generate a calendar by using month and year.
+     * found on https://web.cs.dal.ca/~jamie/CS3172/Course/assig/zeller.html#zeller
+     * weekday = ((13 × month − 1) ÷ 5 + year ÷ 4 + century ÷ 4 + day + year − 2 × century) % 7
+     * @param year
+     * @param month
+     */
+    getMonthCalendar(year, month) {
+      this.days = [];
+      let monthDays = 31;
+      if (month === 2) {
+        monthDays = 28 + (year % 4 === 0 && year % 100 !== 0 || year % 400 !== 0 ? 1 : 0);
+      }
+      if ([4, 6, 9, 11].indexOf(month) !== -1) {
+        monthDays = 30;
+      }
+      const century = Math.ceil(year / 100);
+      const firstDay = (Math.ceil((13 * month - 1) / 5) + Math.ceil(year / 4) + Math.ceil(century / 4) + year - 2 * century) % 7;
+      for (let i = 1; i <= firstDay; i += 1) this.days.push(0);
+      for (let i = 1; i <= monthDays; i += 1) this.days.push(i);
+      
+    }
+  },
+  mounted() {
+    const today = new Date();
+    this.getMonthCalendar(today.getFullYear(), today.getMonth() + 2);
+  }
 }
 </script>
 
 <template>
-  <InputBlock v-model="modelValue" placeholder="dd/mm/YYYY HH:MM" ><slot /></InputBlock>
+  <InputBlock @focusin="showPicker=true" v-model="modelValue" placeholder="dd/mm/YYYY HH:MM" ><slot /></InputBlock>
+  <div class="datetime-block">
+    <div v-if="showPicker" class="datetime-calendar">
+
+      <div class="datetime-month">
+        <button class="btn small">&lt;</button>
+        feb
+        <button class="btn small">&gt;</button>
+      </div>
+
+      <div class="datetime-days">
+        <button class="btn small" v-for="(day, key) in days" :key="key" :disabled="day === 0">
+          <span v-if="day !== 0">{{day}}</span>
+          <span v-else>&nbsp;</span>
+        </button>
+      </div>
+
+    </div>
+  </div>
 </template>
 
 <style scoped>
